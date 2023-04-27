45ce67a73d277abff3e3e89a7ec6b4040adba60e
eiidm <luisa.eid@unesp.br>
2023-04-27 14:06:05 -0300

    Update mascaras.js

--- login_empresa/mascaras.js
+++ login_empresa/mascaras.js
@@ -5,7 +5,8 @@
     //$("#cnpj").mask("99.999.999/9999-99");
  // });
 
- function mascaraCNPJ(cnpj) {
+function mascaraCNPJ(cnpj) 
+{
   cnpj = cnpj.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
   cnpj = cnpj.replace(/^(\d{2})(\d)/, '$1.$2'); // Adiciona ponto após os primeiros 2 dígitos
   cnpj = cnpj.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3'); // Adiciona ponto após os próximos 3 dígitos
