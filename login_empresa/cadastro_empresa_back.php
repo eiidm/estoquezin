e6de38c364bf621fb69bc84039cda4bb86eecffd
Brenobizeli <bp.bizeli@unesp.br>
2023-04-27 15:10:05 -0300

    Back cadastro e login
    conexo, cadastro e login

--- login_empresa/cadastro_empresa_back.php
+++ login_empresa/cadastro_empresa_back.php
@@ -1,3 +1,46 @@
 <?php
+include "../utils/conexao.php"; 
+
+// Recuperação de dados
+   $razaosocial=$_POST['razaosocial'];
+   $email=$_POST['email'];
+   $cnpj=$_POST['cnpj'];
+   $endereco=$_POST['endereco'];
+   $telefone=$_POST['telefone'];
+   $senha=$_POST['senha'];
+  
+// Inserção
+   $sql="INSERT INTO nometabelaempresa
+         (id_empresa, razaosocial, email, cnpj, endereco, telefone, senha)
+         VALUES (
+           DEFAULT,
+           '$razaosocial',  
+           '$email', 
+           '$cnpj',
+           '$endereco',
+           '$telefone',
+           '$senha');";
+
+// Execução
+   $resultado=mysqli_query($conecta,$sql);
+   $linhas=mysqli_affected_rows($resultado);
+
+if ($linhas > 0)
+   {
+       echo '<script language="javascript">';
+       echo "alert('Empresa salvo com sucesso!')";
+       echo '</script>';	
+
+       header("Location: cad_empresa_front.php");
+   }   
+   else
+   {
+       echo '<script language="javascript">';
+       echo "alert('Erro na gravação da empresa!')";
+       echo '</script>';	
+   }
+
+   // Fecha a conexão com o MySQL
+   mysqli_close($conecta);
 
 ?>
---
+++ utils/conexao.php
@@ -0,0 +1,17 @@
+<?php
+ $stringdeconexao = "host= mysql.efesonet.com
+                        port=3306
+                        dbname=projetoscti4
+                        user=efesonet08
+                        password=eq173b189";
+
+$conecta = mysqli_connect($stringdeconexao);
+
+if (!$conecta) {
+        echo '<script language="javascript">';
+        echo "alert('Não foi possível estabelecer conexão com o banco de dados!')";
+        echo '</script>';	
+        
+        exit;
+    }
+?>    
