675696cd6f4d5533d792dcb2d9898dde3c405f26
vivieirad <vitoria.vieira@unesp.br>
2023-04-27 14:18:53 -0300

    menu

---
+++ login_empresa/menu.php
@@ -0,0 +1,21 @@
+<div class='cabecalho'>
+            <input id='menu_to' type='checkbox' />
+            <label class='menu_btn' for='menu_to'>
+                <span></span>
+            </label>
+            <ul class='menu_box'>
+                <li><a class='menu_item' href='../../../../BancoV6/index.php'>Home</a></li>
+                <li><a class='menu_item' href='../../../../BancoV6/Cadastros/selecaoProduto/cadastros/cad_pesq_produtos_front.php' target='_parent'>Produtos</a></li>
+                <li><a class='menu_item' href='../../../../BancoV6/Cadastros/Produtos/cadastros/cad_pesq_produtos_front.php' target='_parent'>Cadastro de Produtos</a></li>
+                <li><a class='menu_item' href='../../../../BancoV6/Cadastros/Usuarios/cadastros/cad_pesq_usuarios_front.php' target='_parent'>Cadastro de Usuários</a></li>
+                <li><a class='menu_item' href='../../../../BancoV6/Cadastros/Devs/devs.php'>Devs</a></li>
+                <li><a class='menu_item' href='../../../../BancoV6/Cadastros/estatisticas/estatistica.php'>Estatísticas</a></li>
+                <li><a class='menu_item' href='../../../../BancoV6/Cadastros/patrocinadores/patro.php'>Patrocinadores</a></li>
+            </ul>
+           
+            
+           
+           
+            
+            
+        </div>
