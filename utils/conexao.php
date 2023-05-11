35d947b53b4c67a231eb01a5a516951fe8eb56f6
Brenobizeli <bp.bizeli@unesp.br>
2023-05-11 15:16:25 -0300

    Update conexao.php

--- utils/conexao.php
+++ utils/conexao.php
@@ -1,12 +1,14 @@
 <?php
 
-$host = "mysql.efesonet.com";
-$port="3306"; 
-$dbname="efesonet08"; // endereço do servidor MySQL
+$host = "mysql.efesonet.com"; //nome do servidor do MySQL
 $usuario = "efesonet08"; // nome do usuário do MySQL
 $password = "eq173b189"; // senha do MySQL
+$port="3306"; 
+$database="efesonet08"; // endereço do servidor MySQL
+
+
                         
-$conecta = mysqli_connect($host, $port, $dbname, $usuario, $password);
+$conecta = mysqli_connect($host, $usuario, $password, $port, $database);
 
 if (!$conecta) {
         die("Erro ao conectar com o MySQL: " . mysqli_connect_error());
