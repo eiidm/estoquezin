296082ae6a907ded62e75e532b0b596b85f4c71c
Brenobizeli <bp.bizeli@unesp.br>
2023-06-03 11:02:55 -0300

    tttt

--- utils/conexao.php
+++ utils/conexao.php
@@ -1,19 +1,11 @@
 <?php
 
-$host = "mysql.efesonet.com"; //nome do servidor do MySQL
+$host = "mysql24-farm10.kighost.net"; //nome do servidor do MySQL
 $port=3306; //numero da porta no banco
 $usuario = "efesonet08"; // nome do usuário do MySQL
 $password = "eq173b189"; // senha do MySQL
 $database="efesonet08"; // endereço do servidor MySQL
 
-/*
-
-    $stringdeconexao = "host=mysql.efesonet.com
-                        port=3306
-                        dbname=efesonet08
-                        user=efesonet08
-                        password=eq173b189";
-*/
 
                         
 $conecta = mysqli_connect($host, $usuario, $password, $database, $port);
