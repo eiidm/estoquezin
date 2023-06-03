82e6bd9c65a6a6053644192cc56cb9106ff5309d
vivieirad <vitoria.vieira@unesp.br>
2023-06-03 07:30:51 -0300

    home

---
+++ home/home_front.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../home/home.css">
+    <link rel="stylesheet" href="./menu.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"></script>
+    <script src="https://kit.fontawesome.com/02a8c7e6b8.js" crossorigin="anonymous"></script>
+   <!-- <title>Cadastro de Produtos</title>-->
+</head>
+
+<body>
+    <div class='cabecalho'>
+        <input id='menu_to' type='checkbox' />
+        <label class='menu_btn' for='menu_to'>
+            <span></span>
+        </label>
+            <ul class='menu_box'>
+                <li><a class='menu_item' href=''>Home</a></li>
+                <li><a class='menu_item' href="../login_colaborador/login_colaborador_front.php">Colaborador</a></li>
+
+                <li><a class='menu_topico'>Grupos</a></li>
+                    <li><a class='menu_item' href="../cadastro_grupo/cadastro_grupo_front.php">Cadastro</a></li>
+                    <li><a class='menu_item' href=''>Alteração</a></li>
+                    <li><a class='menu_item' href=''>Exclusão</a></li>
+
+                <li><a class='menu_topico'>Produtos</a></li>
+                    <li><a class='menu_item' href='cadastro_produtos_front.php'>Cadastro</a></li>
+                    <li><a class='menu_item' href=''>Alteração</a></li>
+                    <li><a class='menu_item' href=''>Exclusão</a></li>
+                    <li><a class='menu_item' href='../consulta_produtos/consulta_produtos_front.php'>Consulta</a></li>
+                    <li><a class='menu_item' href=''>Pedido de compra</a></li>
+
+                <li><a class='menu_topico'>Fornecedores</a></li>
+                    <li><a class='menu_item' href='../cadastro_fornecedor/cadastro_fornecedor_front.php'>Cadastro</a></li>
+                    <li><a class='menu_item' href=''>Alteração</a></li>
+                    <li><a class='menu_item' href=''>Exclusão</a></li>
+            </ul>
+        </div>
+
+    <div class='colunadois'>
+        <img scr=''>
+    </div> <!-- ------ MENU ------ -->
+    <div class="corpo">
+            <a name="topo"></a>
+            <div class="Colunaum">
+                <div class="descricao"> 
+                    <h1 class="desc_txt2">Organize seu estoque</h1>
+                    <p class="desc_txt1">No mercado atual, as empresas necessitam se manter competitivas, frente aos seus concorrentes.
+                        Assim, a gestão de materiais promove um equilíbrio que contribui para a sustentabilidade organizacional.</p>
+
+                    <div class="desc_btn"><a href="">Faça o cadastro</a></div>
+        
+             </div>
+        </div>
+            <br>
+            <div class="prod_imgs">
+
+                <div class="prod1">
+                    <div class="prod_txt1">
+                        <p class="prod_txt">
+                            O totem responsável por registrar momentos de forma fisica.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="prod2">
+                
+                    <div class="prod_txt1">
+                        <p class="prod_txt">
+                            Tire foto com amigos para que possam se lembrar do dia.
+                        </p>
+                    
+                    </div>
+                </div>
+                <div class="prod3">
+               
+                    <div class="prod_txt1">
+                        <p class="prod_txt">
+                            E por fim tenha registrado de forma fisica.
+                         </p>
+                    </div>
+                </div>
+            </div>
+            <br><br>
+
+        </div>
+   
+  
+
+
+</body>
+</html>
