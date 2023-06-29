cd0921f231082fe7ace13adbce50a07265161e7b
Brenobizeli <bp.bizeli@unesp.br>
2023-06-29 14:46:13 -0300

    Update conexao.php

--- utils/conexao.php
+++ utils/conexao.php
@@ -1,12 +1,12 @@
 <?php
 
-$host = "mysql24-farm10.kinghost.net"; //nome do servidor do MySQL
-$port=3306; //numero da porta no banco
-$usuario = "efesonet08"; // nome do usuário do MySQL
-$password = "eq173b189"; // senha do MySQL
-$database="efesonet08"; // endereço do servidor MySQL
+define('host', 'mysql24-farm10.kinghost.net');
+define('usuario', 'efesonet08');
+define('password', 'eq173b189');
+define('database', 'efesonet08');
+define('port', 3306);
 
-$conecta = mysqli_connect($host, $usuario, $password, $database, $port);
+$conecta = new MySQLi(host, usuario, password, database, port );
 
 if (!$conecta) {
     echo "Error: Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
