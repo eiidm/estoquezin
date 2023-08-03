5bdc0d5debb0b6a808fbe529b3a7a87974e07cd1
Brenobizeli <bp.bizeli@unesp.br>
2023-08-03 15:05:40 -0300

    Update alterar_fornecedores_front.php

--- acoes_fornecedor/alterar_fornecedores_front.php
+++ acoes_fornecedor/alterar_fornecedores_front.php
@@ -87,7 +87,7 @@
                     print "<td>".$row->number."</td>";
                     print "<td>
                             <div class='btn_ver'>
-                               <button> <a class='texto_btn' location.href='?page=editar&id_fornecedor='.$row->id_fornecedor'>Alterar Dados</a> </button>
+                                <a class='texto_btn' href='editar.php'>Alterar Dados</a>
                             </div>
                             </td>";
                     print "</tr>";                                 
