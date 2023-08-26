d3e59a4dffd300fb92896968719b5662fd3c041a
vivieirad <vitoria.vieira@unesp.br>
2023-08-26 07:33:32 -0300

    item_cadastro

---
+++ item/cadastro_item_front.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./cadastro_item.css">
+    <link rel="stylesheet" href="./menu.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://kit.fontawesome.com/02a8c7e6b8.js" crossorigin="anonymous"></script>
+    <title>Cadastro de Fornecedores</title>
+</head>
+
+<body>
+    <div class='cabecalho'>
+        <input id='menu_to' type='checkbox' />
+        <label class='menu_btn' for='menu_to'>
+            <span></span>
+        </label>
+            <ul class='menu_box'>
+                <li><a class='menu_item' href='../home/home_front.php'>Home</a></li>
+                <li><a class='menu_item' href="../login_colaborador/login_colaborador_front.php">Colaborador</a></li>
+
+                <li><a class='menu_topico'>Empresas</a></li>
+                    <li><a class='menu_item' href='../login_empresa/cadastro_empresa_front.php'>Cadastro</a></li>
+                    <li><a class='menu_item' href='../acoes_empresa/alterar_empresas_front.php'>Alteração/Exclusão</a></li>
+
+                <li><a class='menu_topico'>Grupos</a></li>
+                    <li><a class='menu_item' href="../cadastro_grupo/cadastro_grupo_front.php">Cadastro</a></li>
+                    <li><a class='menu_item' href='../acoes_grupo/alterar_grupos_front.php'>Alteração/Exclusão</a></li>
+
+                <li><a class='menu_topico'>Produtos</a></li>
+                    <li><a class='menu_item' href='../cadastro_produtos/cadastro_produtos_front.php'>Cadastro</a></li>
+                    <li><a class='menu_item' href='../acoes_produtos/alterar_produtos_front.php'>Alteração/Exclusão</a></li>
+                    <li><a class='menu_item' href='../consulta_produtos/consulta_produtos_front.php'>Consulta</a></li>
+                    <li><a class='menu_item' href=''>Pedido de compra</a></li>
+
+                <li><a class='menu_topico'>Fornecedores</a></li>
+                    <li><a class='menu_item' href="./cadastro_fornecedor_front.php">Cadastro</a></li>
+                    <li><a class='menu_item' href='../acoes_fornecedor/alterar_fornecedores_front.php'>Alteração/Exclusão</a></li>
+            </ul>
+        </div>
+
+    <div class='home'>
+        <img scr=''>
+    </div> <!-- ------ MENU ------ -->
+
+    <div class="container">
+        <br> <br> 
+        <h2 class="texto texto-um">Cadastro de item</h2><br> 
+        <div class="tela Um">
+            <form class="form" action="./cadastro_fornecedor_back.php" method="post">     
+                <div class="colunaDois">       
+
+                    <label class="label-input" for="">
+                        <input type="text" name="valoraqusicao" placeholder="Valor Aquisição" required>
+                    </label>
+
+                    <label class="label-input" for="">
+                        <input type="text" name="valorvenda" placeholder=" Valor de venda" required> 
+                    </label>
+
+                    <label class="label-input" for="">
+                        <input type="text" name="saldoitem" placeholder="Saldo item" required> 
+                    </label>
+
+
+                   
+                    <button class="btn btn-dois">Cadastrar item</button>
+                </div>  <!--coluna dois-->
+            </form>   
+            
+            
+
+
+        </div>  <!--tela um-->
+    </div><!--container-->
+
+</body>
+</html>
