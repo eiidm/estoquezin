9c84fca2930aff7c0aba291272fa28696fccb14c
Brenobizeli <bp.bizeli@unesp.br>
2023-09-28 15:20:20 -0300

    lohfjfmfmf

--- login_empresa/logoff_empresa.php
+++ login_empresa/logoff_empresa.php
@@ -1,12 +1,12 @@
 <?php
     session_start();
-
-    if (isset($_SESSION["empresalogada"]))
-    {
-        session_unset();
-        session_destroy();
-    }
-
-    echo "<meta HTTP-EQUIV='refresh' CONTENT='0;URL=../index.php'>";
+    unset( $_SESSION["razaosocial"]);
+    unset(  $_SESSION["email"]);
+    unset( $_SESSION["cnpj"]);
+    unset( $_SESSION["endereco"]);
+    unset(  $_SESSION["telefone"]);
+    unset(  $_SESSION["senha"]);
+    session_destroy();
+    header("Location: login_empresa_front.php");
     exit;
 ?>
